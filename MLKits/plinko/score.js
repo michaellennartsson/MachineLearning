63f499460126c558a165b920c42074a4fb5494fb
Michael Lennartsson <michael.lennartsson@cybercom.com>
2018-12-07 17:11:25 +0100

    Initial commit KNN

---
+++ MLKits/plinko/score.js
@@ -0,0 +1,77 @@
+const outputs = [];
+
+const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => {
+  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+};
+
+const runAnalysis = () => {
+  // Write code here to analyze stuff
+  const testSetSize = 50;
+  const [testSet, trainingSet] = splitDataSet(minMax(outputs, 3), testSetSize);
+
+  _.range(1, 15).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(
+        testObservation =>
+          knn(trainingSet, _.initial(testObservation), k) === testObservation[3]
+      )
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log('For k of', k, 'accuracy is: ', accuracy);
+  });
+};
+
+const knn = (data, point, k) => {
+  return _.chain(data)
+    .map(row => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+};
+
+const distance = (pointA, pointB) => {
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
+};
+
+const splitDataSet = (data, testCount) => {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+};
+
+const minMax = (data, featureCount) => {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+
+    return clonedData;
+  }
+};
